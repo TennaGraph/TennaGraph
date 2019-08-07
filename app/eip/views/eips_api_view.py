13c53b87e365ac9f921ce3f6eac107e49e431ad2
Malkevych Bohdan <bomalkevych@gmail.com>
2019-08-07 12:47:17 +0300

    added filtering eips by is_voting_enabled and updated_after_timestamp

--- app/eip/views/eips_api_view.py
+++ app/eip/views/eips_api_view.py
@@ -2,12 +2,17 @@
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import serializers
+from django.db.models import Subquery
 
 # App imports
 from ..serializers import EIPSerializer
 from ..models import EIP
 from base import date_utils
+from ethereum_client.models import VotingDetailsLog
 
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
 class EIPsAPIView(generics.ListAPIView):
 
@@ -25,8 +30,14 @@ class EIPsAPIView(generics.ListAPIView):
         if updated_after_timestamp is not None:
             try:
                 updated_at = date_utils.utcfromtimestamp(int(updated_after_timestamp))
-                queryset = self.queryset.filter(updated_at__gt=updated_at)
+                queryset = queryset.filter(updated_at__gt=updated_at)
             except Exception as err:
                 raise serializers.ValidationError("Parameter `updated_after_timestamp` must be in the timestamp format (10 digits). Error: {}".format(err))
+
+        is_voting_enabled = self.request.query_params.get('is_voting_enabled', None)
+        if is_voting_enabled is not None:
+            is_voting_enabled = str2bool(is_voting_enabled)
+            queryset = queryset.filter(voting_details__isnull=False)\
+                .filter(voting_details__in=Subquery(VotingDetailsLog.objects.filter(is_voting_open=is_voting_enabled).values('pk')))
 
         return queryset.all()
